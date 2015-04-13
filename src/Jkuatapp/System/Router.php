4f339f71ae5b713e65cd702ee83cea7be0056bae
Gocho Mugo <mugo@forfuture.co.ke>
2015-04-13 17:54:09 +0300

    add start function for router

--- src/Jkuatapp/System/Router.php
+++ src/Jkuatapp/System/Router.php
@@ -4,26 +4,37 @@
 namespace Jkuatapp\System;
 
 
-//use Luracast\Restler\Restler;
+use Luracast\Restler\Restler;
 
 
-class Router {
+class Router
+{
+    protected $restlerRouter;
 
-  protected $restlerRouter;
+    /**
+    * Constructs our system router
+    */
+    public function __construct()
+    {
+        $this->restlerRouter = new Restler();
+    }
 
-  /**
-  * Constructs our system router
-  */
-  public function __construct() {
-    //$this->restlerRouter = new Restler();
-  }
+    /**
+    * Register module to add its endpoints
+    */
+    public function registerModule()
+    {
 
-  /**
-  * Allow module to add its endpoints
-  */
-  public function addModule() {
-  
-  }
+    }
+
+
+    /**
+    * Starts the Server listening
+    */
+    public function start()
+    {
+        $this->restlerRouter->handle();
+    }
 
 }
 
---
+++ tests/unit/RouterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+
+use Jkuatapp\System as System;
+
+
+class RouterTest extends PHPUnit_Framework_TestCase
+{
+  public function testRegisterModule()
+  {
+    
+  }
+}
